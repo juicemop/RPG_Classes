25d6e3535d8b710e08a3938ab4020700557836a7
juicemop <jeezusmod@gmail.com>
2025-06-25 11:51:55 -0400

    Update PaladinBuff.java

--- src/main/java/rpgclasses/buffs/principalability/PaladinBuff.java
+++ src/main/java/rpgclasses/buffs/principalability/PaladinBuff.java
@@ -10,8 +10,10 @@ public class PaladinBuff extends SimpleClassBuff {
         super(
                 new ModifierValue<>(BuffModifiers.ARMOR, 0.5F),
                 new ModifierValue<>(AphModifiers.MAGIC_HEALING_RECEIVED, 0.5F),
-                new ModifierValue<>(BuffModifiers.SPEED, -0.25F),
-                new ModifierValue<>(BuffModifiers.ATTACK_SPEED, -0.5F)
+                new ModifierValue<>(AphModifiers.MAGIC_HEALING, 1F),
+                new ModifierValue<>(BuffModifiers.MAX_HEALTH, 1F),
+                new ModifierValue<>(BuffModifiers.HEALTH_REGEN, 1F),
+                new ModifierValue<>(BuffModifiers.COMBAT_HEALTH_REGEN, 1F)
         );
     }
 }
