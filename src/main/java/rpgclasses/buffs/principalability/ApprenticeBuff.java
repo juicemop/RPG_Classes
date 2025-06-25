24030d0a8c7370b036d5633da24be9c7fad1bc62
juicemop <jeezusmod@gmail.com>
2025-06-25 11:48:20 -0400

    Update ApprenticeBuff.java

--- src/main/java/rpgclasses/buffs/principalability/ApprenticeBuff.java
+++ src/main/java/rpgclasses/buffs/principalability/ApprenticeBuff.java
@@ -7,10 +7,10 @@ import rpgclasses.buffs.SimpleClassBuff;
 public class ApprenticeBuff extends SimpleClassBuff {
     public ApprenticeBuff() {
         super(
+                new ModifierValue<>(BuffModifiers.MAGIC_DAMAGE, 2F),
                 new ModifierValue<>(BuffModifiers.MAX_MANA, 1F),
                 new ModifierValue<>(BuffModifiers.MANA_REGEN, 1F),
-                new ModifierValue<>(BuffModifiers.ARMOR, -0.5F),
-                new ModifierValue<>(BuffModifiers.MAX_HEALTH, -0.25F)
+                new ModifierValue<>(BuffModifiers.ATTACK_MOVEMENT_MOD, 0F)
         );
     }
 }
